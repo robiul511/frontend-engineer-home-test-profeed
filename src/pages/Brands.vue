40f49e3502e282fbe6ed7ed2535a1272d9269503
robiul511 <robiultriawali@gmail.com>
2023-06-29 20:35:13 +0700

    add router in each page

--- src/pages/Brands.vue
+++ src/pages/Brands.vue
@@ -7,6 +7,8 @@ console.log(brandStores);
 </script>
 
 <template>
+
+  <router-link to="/">Back to cars page </router-link>
   <h2> Brands Page </h2>
   <p>{{ brandStores.dataBrands }}</p>
 </template>
--- src/pages/Cars.vue
+++ src/pages/Cars.vue
@@ -1,16 +1,42 @@
 <script setup lang="ts">
 import { useCarsStore } from '../stores/cars.store'
 
+import { useBrandsStore } from '../stores/brands.store'
+
+const brandStores = useBrandsStore()
+
 const carStores = useCarsStore()
+
 console.log(carStores);
+
+const addBrands = () => {
+  const payload = {
+    id: 2,
+    name: 'honda'
+  }
+
+  brandStores.addBrand(payload)
+}
 
 
 </script>
 
 <template>
+  <div class="button-flex">
+    <button @click="addBrands">Add new brands</button>
+    <router-link to="/brands">Go to brands page </router-link>
+
+  </div>
   <h2> Cars Page </h2>
+  <p>data cars</p>
   <p>{{ carStores.dataCars }}</p>
+  <p>data brands</p>
+  <p>{{ brandStores.dataBrands }}</p>
 </template>
 
 <style scoped>
+.button-flex {
+  display: flex;
+}
+
 </style>
